6d3c8c3511bd2622d7b4eb2824035b5a82b1ee54
Syafri <syafrialiputra@gmail.com>
2023-06-11 11:06:30 +0700

    first commit

---
+++ helpers/Menu.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'table1', 
+			'label' => 'Table1', 
+			'icon' => ''
+		)
+	);
+		
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'table1', 
+			'label' => 'Table1', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
